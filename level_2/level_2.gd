86a74fdac271819a4b1888ce76b8c66c0e73a7eb
EricElMagenta <fabian.lolli@gmail.com>
2026-01-31 03:56:45 -0300

    relleno

--- level_2/level_2.gd
+++ level_2/level_2.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+@onready var time = %Time
+@onready var timer = $Timer
+
+func _ready():
+    time.text = str(timer.wait_time)
+
+func _process(_delta):
+    time.text = str(round(timer.time_left))
