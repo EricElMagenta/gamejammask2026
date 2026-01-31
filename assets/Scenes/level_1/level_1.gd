ce488aa73fd4d060eb9e1b0142ed2ce8e524f14d
nzunigav <technicodj@gmail.com>
2026-01-31 16:24:22 -0300

    Timer funcionando con contador en pantalla

--- assets/Scenes/level_1/level_1.gd
+++ assets/Scenes/level_1/level_1.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+@onready var label_tiempo: Label = $CanvasLayer/LabelTiempo
+@onready var label_items: Label = $CanvasLayer/LabelItems
+@onready var timer: Timer = $Timer
+
+var time_left := 3
+var items_count := 0
+
+func _ready():
+	label_tiempo.text = "Tiempo: %d" % time_left
+	label_items.text = "Items: %d" % items_count
+
 func _on_timer_timeout() -> void:
-	print("⏱️ timeout en level_1")
-	get_tree().change_scene_to_file("res://assets/Scenes/level_2/level_2.tscn")
+	time_left -= 1
+	label_tiempo.text = "Tiempo: %d" % time_left
+
+
+
+	if time_left <= 0:
+		timer.stop()
+		get_tree().change_scene_to_file("res://assets/Scenes/level_2/level_2.tscn")
