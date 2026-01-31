3ced7326a66c95df61db7ac25c9c86145f5797df
Ricky <r.rojas78@alumnos.santotomas.cl>
2026-01-30 23:38:11 -0300

    Prototipo de funcionamiento basico del microjuego incluye cambio de sprites, contador, condicion de victoria y confirmacion de victoria

---
+++ node_2d.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var contador = 0
+var puntos_victoria = 30
+var success = false
+@onready var label = $Label # Ajusta $Label al nombre de tu nodo Label
+@onready var label_victoria = $RichTextLabel # Label enriquecido
+
+func _process(_delta):
+	# Detecta si la tecla se presiona solo una vez por pulsación
+	if Input.is_action_just_pressed("presionar_tecla"):
+		contador += 1
+		actualizar_contador()
+		condicion_victoria()
+
+#Funciones
+
+func mostrar_texto_victoria():
+	label_victoria.visible = true # Hace visible el nodo
+	#label_victoria.text = "¡Condición cumplida!" # Cambia el texto si es necesario
+
+func condicion_victoria():	
+	if contador == puntos_victoria:
+		success = true
+		mostrar_texto_victoria()
+
+func actualizar_contador():
+	label.text = "Contador: " + str(contador)
+	print("Contador actual: ", contador) # Opcional: ver en consola             
---
+++ sprite_2d.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+# Carga las dos texturas en el script
+var textura1 = preload("res://assets/idle.png")
+var textura2 = preload("res://assets/pressed.png")
+
+func _ready():
+	texture = textura1 # Textura inicial
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"): # "ui_accept" es espacio por defecto
+		texture = textura2
+	elif event.is_action_released("ui_accept"):
+		texture = textura1
