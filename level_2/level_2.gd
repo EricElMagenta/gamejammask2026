f7a671a751fea12a149a631669134bb1a4e042ea
EricElMagenta <fabian.lolli@gmail.com>
2026-01-31 11:30:59 -0300

    Merge con menú de inicio, nivel 2 y nivel 3

--- level_2/level_2.gd
+++ level_2/level_2.gd
@@ -1,10 +1,41 @@
 extends Node2D
 
+@export var next_scene:PackedScene
+@export var n_areas := 1
+
 @onready var time = %Time
 @onready var timer = $Timer
+@onready var result_label = $ResultLabel
+
+@onready var dragable_piece = $DragablePiece
+
+@onready var fillable_area = $FillableArea
+
+const MIN_SCORE_REQUIRED = 80
 
 func _ready():
     time.text = str(timer.wait_time)
 
 func _process(_delta):
     time.text = str(round(timer.time_left))
+    if get_mean_score() >= 100: 
+        show_results()
+
+func get_mean_score():
+    var score := 0.0
+    score += fillable_area.filled_percentage
+    return score / n_areas
+
+func show_results():
+    timer.stop()
+
+    dragable_piece.dragging = false
+
+    var mean_score = get_mean_score()
+    if mean_score > MIN_SCORE_REQUIRED: result_label.text = "ÉXITO"
+    else: result_label.text = "¡PERDEDOR!"
+    await get_tree().create_timer(1).timeout
+    get_tree().change_scene_to_packed(next_scene)
+
+func _on_timer_timeout():
+    show_results()
--- startmenu/startmenu.gd
+++ startmenu/startmenu.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 @export var next_scene:PackedScene
 @onready var bg = $Background
+@onready var next_game_btn = $Background/NextGameBtn
 
 func _ready():
-	bg.frame = 0 
-
+	bg.frame = 0
 
 func _on_button_pressed():
 	bg.frame = 1
+	next_game_btn.disabled = false
 
+func _on_next_game_btn_pressed():
+	if next_scene: get_tree().change_scene_to_packed(next_scene)
