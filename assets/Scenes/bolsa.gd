3a458b8ae03368f7bde85488ae8c64450dbefedf
nzunigav <technicodj@gmail.com>
2026-01-31 14:22:16 -0300

    Contador UI Funcionando

--- assets/Scenes/bolsa.gd
+++ assets/Scenes/bolsa.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+@export var label_contador: Label
 var contador_items := 0
+
 
 func agregar_item():
 	contador_items += 1
-	print("Items en la bolsa:", contador_items)
+	label_contador.text = str(contador_items)
