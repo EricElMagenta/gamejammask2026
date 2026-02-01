9d3bb8cf30016f358b7c5a72c825f7bf157cd152
nzunigav <technicodj@gmail.com>
2026-02-01 00:46:52 -0300

    Colocacion texto introduccion agregando fuente personalizada y animacion

--- startmenu/startmenu.gd
+++ startmenu/startmenu.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var next_game_btn = $Background/NextGameBtn
 @onready var title_label = $Background/Title
 
+
 func _ready():
 	AudioManager.play_music()
 	GameManager.total_score = 0
@@ -13,7 +14,13 @@ func _ready():
 func _on_button_pressed():
 	title_label.visible = false
 	bg.frame = 1
+
+	$Background/HistoriaLabel.visible = true
 	next_game_btn.disabled = false
+
+	
+	var tween = create_tween()
+	tween.tween_property($Background/HistoriaLabel, "visible_ratio", 1.0, 8.0).from(0.0)
 
 func _on_next_game_btn_pressed():
 	if next_scene: get_tree().change_scene_to_packed(next_scene)
